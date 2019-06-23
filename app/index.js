37e54ec9ff0f5110c5ac8f0121c7f66c87db1a5d
fancn <fanyj@yikeer.com>
2019-06-23 19:18:03 +0800

    payload recieving

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@
 // Dependencies
 var http = require("http");
 var url = require("url");
+var StringDecoder = require("string_decoder").StringDecoder;
 
 // Configure the server to respond to all requests with a string
 var server = http.createServer(function(req, res) {
@@ -24,6 +25,22 @@ var server = http.createServer(function(req, res) {
 
   //Get the headers as an object
   var headers = req.headers;
+
+  // Get the payload,if any
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on("end", function() {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end("Hello World!\n");
+
+    // Log the request/response
+    console.log("Request received with this payload: ", buffer);
+  });
 
   // Send the response
   res.end("Hello World!\n");
